3d85c6d593f806612d978ff2d2df320dd1060442
Rafael Nogueira Rabelo <rafaelnrabelo@gmail.com>
2020-08-13 08:58:43 -0300

    .

--- src/pacientes/Paciente.java
+++ src/pacientes/Paciente.java
@@ -2,7 +2,7 @@ package pacientes;
 
 import java.time.LocalTime;
 
-public abstract class Paciente {
+public abstract class Paciente implements Comparable<Paciente> {
   private String nome;
   private LocalTime horario_chegada;
   private LocalTime horario_atendimento;
@@ -14,6 +14,10 @@ public abstract class Paciente {
     this.numero_vacinas = numero_vacinas;
   }
 
+  public LocalTime getHorario_chegada() {
+    return horario_chegada;
+  }
+
   public void setHorarioAtendimento(LocalTime horario_atendimento) {
     this.horario_atendimento = horario_atendimento;
   }
@@ -21,6 +25,11 @@ public abstract class Paciente {
   @Override
   public String toString() {
     return "Nome: " + nome + "\tNúmero de Vacinas: " + numero_vacinas + "\nHorario de Chegada: " + horario_chegada
-        + "\tHorario de Atendimento: " + horario_atendimento;
+        + "\tHorario de Atendimento: " + horario_atendimento + "\n";
+  }
+
+  @Override
+  public int compareTo(Paciente paciente2) {
+    return this.getHorario_chegada().compareTo(paciente2.getHorario_chegada());
   }
 }
