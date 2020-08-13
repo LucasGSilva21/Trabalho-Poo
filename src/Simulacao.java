8de9ff45e651ce47d5a2912fff98037c7b9b0a5c
LucasGSilva21 <lucasgsilva2102@gmail.com>
2020-08-12 21:37:33 -0300

    criando

--- src/Simulacao.java
+++ src/Simulacao.java
@@ -1,6 +1,19 @@
+import pacientes.*;
+
 public class Simulacao {
 
   public Simulacao() {
 
+  }
+
+  public void getPaciente() {
+    // Paciente novoPaciente;
+
+    // if fila prioritaria
+    // novoPaciente =
+    // else fila normal
+    // novoPaciente =
+
+    // return novoPaciente;
   }
 }
