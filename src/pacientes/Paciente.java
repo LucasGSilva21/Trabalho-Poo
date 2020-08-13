0b1f142b17af24d36f58c57266fa3149670272a7
Rafael Nogueira Rabelo <rafaelnrabelo@gmail.com>
2020-08-12 22:24:58 -0300

    .

--- src/pacientes/Paciente.java
+++ src/pacientes/Paciente.java
@@ -7,11 +7,13 @@ public abstract class Paciente {
   private LocalTime horario_chegada;
   private LocalTime horario_atendimento;
   private boolean atendido;
+  private int numero_vacinas;
 
-  public Paciente(String nome, LocalTime horario_chegada) {
+  public Paciente(String nome, LocalTime horario_chegada, int numero_vacinas) {
     this.atendido = false;
     this.nome = nome;
     this.horario_chegada = horario_chegada;
+    this.numero_vacinas = numero_vacinas;
   }
 
   public void setHorarioAtendimento(LocalTime horario_atendimento) {
